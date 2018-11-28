d9e3030cbe04542cdb5898d340eba4c622d2e68b
TobiasMie <TobiasMie@users.noreply.github.com>
2018-11-28 13:16:55 +0100

    Fixed #4
    Now the data reaches the GameManager

--- DataManager.js
+++ DataManager.js
@@ -21,32 +21,46 @@ function DataManager(gameManager){
             this.sendedData.push(randNum);
         }
 
-        var json = "{"
-					+"\"number\":\""+randNum+"\","
-					+"\"receiver\": \""+receiver+"\","
-					+"\"type\": \""+type+"\","
-					+"\"data\": \""+data+"\""
-					+"}";
+        // var json = '{'
+		// 			+'"number":"'+randNum+'",'
+		// 			+'"receiver": "'+receiver+'",'
+		// 			+'"type": "'+type+'",'
+		// 			+'"data": "'+data+'"'
+		// 			+'}';
 
         //send as JSON
-		console.log(json);
-		this.websocket.connection.send(json);
+		// console.log(json);
+		this.websocket.connection.send(
+			JSON.stringify({ number: randNum, receiver: receiver, type: type, data: data}));
+			console.log(JSON.stringify({ number: randNum, receiver: receiver, type: type, data: data}));
+		// this.websocket.connection.send(json);
 	}
 
 	this.receiveData = function(input){
+
+		if(!(typeof(input) === 'object')){
+			try {
+				var json = JSON.parse(input);
+			} catch (e) {
+				console.log('Invalid JSON: ', input);
+				console.log(e);
+				return;
+			}
+		}
+
 		//if typ == "Reply" -> execute gameManager.receiveReply(data);
-		if(input.type === "Reply"){
-			gameManager.receiveReply(input.data);
+		if(json.type === "Reply"){
+			gameManager.receiveReply(json.data);
 		}
 		//if typ == "Ask" && number in sendedData[] -> execute gameManager.receiveResult(data)
-		var index = this.sendedData.indexOf(input.number);
-        if(input.type === "Ask" && index != -1){
-			gameManager.receiveResult(input.data);
+		var index = this.sendedData.indexOf(json.number);
+        if(json.type === "Ask" && index != -1){
+			gameManager.receiveResult(json.data);
             this.sendedData.splice(index, 1);
 		}
 		//if typ == "Ask" && number NOT in sendedData[] -> execute gameManager.receiveQuestion(data)
-		if(input.type === "Ask" && index == -1){
-			gameManager.receiveQuestion(input.data, input.number);
+		if(json.type === "Ask" && index == -1){
+			gameManager.receiveQuestion(json.data, json.number);
 		}
 	}
 }
