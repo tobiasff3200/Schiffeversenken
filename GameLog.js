1ffd06e8a721081f7d5f9f9dc2294cbea6eac6b5
CrazyMan2000 <45339639+CrazyMan2000@users.noreply.github.com>
2018-12-04 14:12:01 +0100

    Add colors in GameLog

--- GameLog.js
+++ GameLog.js
@@ -20,27 +20,37 @@ function GameLog(x, y, wid, heig){
         push();
         //übersetzt den Ursprung auf die Mitte des Feldes
         translate(x+wid/2, y+heig/2);
-        //setzt die Textanlehnung auf Links und Unten
-        textAlign(CENTER, BOTTOM);
-        //es soll normal geschrieben werden
-        textStyle(NORMAL);
                
         //print chatField
         fill(120, 100);
         rectMode(CENTER);
         rect(0, 0, wid, heig);
 
-
+        noStroke();
         fill(255, 150);
         //print chat
         //für jeden inhalt im Chat eine neue Zeile
         for(var i = 0; i < this.chat.length; i++){
             //darf icht größer sein als das TextField
             if(heig/1.8-(this.heightT)*(this.chat.length-i) > -heig/3){
-                textAlign(this.chat[i][1]%2==0 ? RIGHT : LEFT);
+                if(i == this.chat.length-1){
+                    //der letzte Zug soll farbig gekennzeichnet sein
+                    fill(255, 0, 255, 240);
+                    //& in der Mitte stehen
+                    textAlign(CENTER, BOTTOM);
+                    //& fett gedruckt sein
+                    textStyle(BOLD);
+                }else{
+                    //macht die letzten Züge grau
+                    fill(255, this.chat[i][1]%2==0 ? 220 : 130);
+                    //& setzt des Spielers nach links und die des Gegners nach Rechts
+                    textAlign(this.chat[i][1]%2==0 ? RIGHT : LEFT, BOTTOM);
+                    //& schreibt sie normal
+                    textStyle(NORMAL);
+                }
                 text(this.chat[i][0], 0, heig/2.1-(this.heightT)*(this.chat.length-i), wid-20, this.heightT);
             }
-        }
+        }        
         //übersetzte den ursprung zurück auf 0,0
         translate(0, 0);
         pop();
@@ -51,7 +61,7 @@ function GameLog(x, y, wid, heig){
     this.postMsg = function(x, y, result, turn){
         var xString = (x<fieldNumbers.length) ? fieldNumbers[x] : null;
         var yString = (y<fieldChars.length) ? fieldChars[y] : null;
-        var playerS = (turn%2==0) ? "You" : "The enemy";
+        var playerS = (turn%2==0) ? "You" : "Enemy";
         var resultS = (result == DESTROYED) ? "destroyed" : (result == HIT) ? "hit" : "miss";
         var newMsg = [playerS + " shot at " + yString + " " + xString + " and " + resultS + " a ship", turn];
         this.chat.push(newMsg);
