e23be07afe5ea381c51a41e69de1b73eaea683d8
CrazyMan2000 <45339639+CrazyMan2000@users.noreply.github.com>
2018-12-13 13:15:41 +0100

    Computer calcBestMove

--- Computer.js
+++ Computer.js
@@ -2,12 +2,11 @@ function Computer(){
     const FIELDSIZE = 10;
     
     //neue Konstanten für den Computer
-    const EMPTYFIELD = 49;  //leeres Feld           -> unklar
-    const POSHIT = 50;      //ein möglicher Treffer -> vermutung
-    const DEFMISS = 51;     //defenitiv Vorbei      -> sicher (ausschlussverfahren)
+    const EMPTYFIELD = 0;  //leeres Feld           -> unklar
+    const DEFMISS = -3;    //defenitiv Vorbei      -> sicher (ausschlussverfahren)
     //nur zur Erinnerung
-    const HIT = HIT;        //getroffene Schüsse    -> sicher
-    const MISS = MISS       //nicht getroffen       -> sicher
+    const HITState = -2;   //getroffene Schüsse    -> sicher
+    const MISSState = -1;  //nicht getroffen       -> sicher
     
     //ist zum speichern und senden der Daten des Computers zuständig
     this.gameManager = null;
@@ -24,13 +23,8 @@ function Computer(){
     this.setup = function(){   
         this.gameManager = new GameManager();
         this.gameManager.setup();
-        //initialisiert alle Felder als leeres Feld ()
-        for(var i = 0; i < FIELDSIZE; i++){
-			this.positionsToShoot[i] = [];
-            for(var j = 0; j < FIELDSIZE; j++){
-                this.positionsToShoot[i][j] = EMPTYFIELD;
-            }
-		}
+        //initialisiert alle Felder als leeres Feld
+        this.stateField = this.convertGamesFieldStatesInComputerStateField(gameManager.gameFields[0].fieldStates);
     }
     
     //Shoot functions
@@ -40,27 +34,29 @@ function Computer(){
     
        
     // auf die Felder um ein zerstörtes Schiff kann man auf keinen fall treffen 
-    this.fieldAroundDestroyedShip = function(){
+    this.setFieldAroundDestroyedShipDefMiss = function(){
         for(var pos of this.lastShipDestroyed){
            this.setDEFMISSAroundHit(pos.x, pos.y); 
         }
     }
     
-    this.setDEFMISSAroundHit = function(x, y){
-        var currField = this.stateField[x+1][y];
-        this.stateField[x+1][y] = (currField != MISS && currField != HIT) ? DEFMISS : EMPTYFIELD; 
-        currField = this.stateField[x-1][y];
-        this.stateField[x-1][y] = (currField != MISS && currField != HIT) ? DEFMISS : EMPTYFIELD;
-        currField = this.stateField[x][y+1];
-        this.stateField[x][y+1] = (currField != MISS && currField != HIT) ? DEFMISS : EMPTYFIELD;
-        currField = this.stateField[x][y-1];
-        this.stateField[x][y-1] = (currField != MISS && currField != HIT) ? DEFMISS : EMPTYFIELD;   
-    }
-    
-    
-    this.areaToSmallForShips = function(){
+    //auf Bereiche, die zu klein für das kleinste Schiff sind kann man kein Schiff treffen
+    this.setAreaToSmallForShipsDefMiss = function(){
         this.setDEFMISSArea(this.shipsAliveLength[0]);
     }
+    
+    //legt die Wahrscheinlichkeit fest die jedes Feld hat dass ein Schiff auf ihm steht
+    this.setValueForPossbileHit = function(){
+        for(var x = 0; i < FIELDSIZE; x++){
+            for(var y = 0; y < FIELDSIZE; y++){
+                this.setPossibleHitForAllShips(x, y);
+            }
+        }
+    }
+
+    
+//-----------------Hilfs-Methoden------------------------------------------------------------------//
+    
     
     this.setDEFMISSArea = function(areaSize){
         for(var x in this.stateField){
@@ -70,6 +66,17 @@ function Computer(){
                 }
             }
         }
+    }  
+    
+    this.setDEFMISSAroundHit = function(x, y){
+        var currField = this.stateField[x+1][y];
+        this.stateField[x+1][y] = (currField != MISSState && currField != HITState) ? DEFMISS : EMPTYFIELD; 
+        currField = this.stateField[x-1][y];
+        this.stateField[x-1][y] = (currField != MISSState && currField != HITState) ? DEFMISS : EMPTYFIELD;
+        currField = this.stateField[x][y+1];
+        this.stateField[x][y+1] = (currField != MISSState && currField != HITState) ? DEFMISS : EMPTYFIELD;
+        currField = this.stateField[x][y-1];
+        this.stateField[x][y-1] = (currField != MISSState && currField != HITState) ? DEFMISS : EMPTYFIELD;   
     }
     
     this.shipNotPossibleToPlace = function(x, y, areaSize){
@@ -90,7 +97,7 @@ function Computer(){
             if(i < 0){
                 return 0;
             }else       
-            if(this.stateField[i][y] == MISS || this.stateField[i][y] == DEFMISS){
+            if(this.stateField[i][y] == MISSState || this.stateField[i][y] == DEFMISS){
                 return i+1;
             }
         }
@@ -99,11 +106,11 @@ function Computer(){
     
     //gibt die Position des ersten NICHT validen Feldes zurück
     this.calcRightRowEnd = function(x, y, areaSize){
-        for(var i = x+1; i <= x+areaSize; i--){
+        for(var i = x+1; i <= x+areaSize; i++){
             if(i >= FIELDSIZE){
                 return FIELDSIZE;
             }else
-            if(this.stateField[i][y] == MISS || this.stateField[i][y] == DEFMISS){
+            if(this.stateField[i][y] == MISSState || this.stateField[i][y] == DEFMISS){
                 return i;
             }
         }
@@ -115,7 +122,7 @@ function Computer(){
             if(i < 0){
                 return 0;
             }else       
-            if(this.stateField[x][i] == MISS || this.stateField[x][i] == DEFMISS){
+            if(this.stateField[x][i] == MISSState || this.stateField[x][i] == DEFMISS){
                 return i+1;
             }
         }
@@ -123,14 +130,80 @@ function Computer(){
     }
     
     this.calcBotColEnd = function(x, y, areaSize){
-        for(var i = y+1; i <= y+areaSize; i--){
+        for(var i = y+1; i <= y+areaSize; i++){
             if(i >= FIELDSIZE){
                 return FIELDSIZE;
             }else
-            if(this.stateField[x][i] == MISS || this.stateField[x][i] == DEFMISS){
+            if(this.stateField[x][i] == MISSState || this.stateField[x][i] == DEFMISS){
                 return i;
             }
         }
         return null;
+    }
+    
+
+//hilfsmethoden für setValueForPossbileHit
+    
+    //prüft ob an der Position x, y und allen die das Schiff an der Position überdecken würde ob
+    //es eine Valide Position ist und erhört den Wert der möglichen Felder
+    this.setPossibleHitForAllShips(x, y){
+        for(var shipSize of this.shipsAliveLength){
+            if(this.checkPossiblePosForShipInRow(x, y, shipSize)){
+                this.increaseValueForPossbileHitInRow(x, y, shipSize);
+            }
+            
+            if(this.checkPossiblePosForShipInCol(x, y, shipSize)){
+                this.increaseValueForPossbileHitInCol(x, y, shipSize);
+            }
+        }
+        
+    }
+    
+    //wenn vom x wert bis zu x+shipSize keine "Hindernis" ist return true
+    this.checkPossiblePosForShipInRow = function(x, y, shipSize){
+        for(var i = x; i < x+shipSize; i++){
+            if(i >= FIELDSIZE || this.stateField[i][y] < EMPTYFIELD){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    //wenn vom y wert bis zu y+shipSize keine "Hindernis" ist return true
+    this.checkPossiblePosForShipInCol = function(x, y, shipSize){
+        for(var i = y; i < y+shipSize; i++){
+            if(i >= FIELDSIZE || this.stateField[x][i] < EMPTYFIELD){
+                return false;
+            }
+        }
+        return true;
+    }
+    
+    //es wurde bereits überprüft ob es eine Valide Position ist
+    this.increaseValueForPossbileHitInRow = function(x, y, shipSize){
+        for(var i = x; i < x+shipSize; i++){
+            this.stateField[i][y]++;
+        }
+    }
+    
+    //es wurde bereits überprüft ob es eine Valide Position ist
+    this.increaseValueForPossbileHitInCol = function(x, y, shipSize){
+        for(var i = y; i < y+shipSize; i++){
+            this.stateField[x][i]++;
+        }
+    }
+    
+    
+    this.convertGamesFieldStatesInComputerStateField = function(var gameFieldState){
+        var computerGameField = gameFieldState.slice();
+        for(var x in gameFieldState){
+            for(var y in gameFieldState[x]){
+                var state = gameFieldState[x][y];
+                if(state != EMPTY){
+                    this.fieldStates[x][y] = (state >= HIT) ? HITState : MISSState;
+                } 
+            }
+        }
+        return computerGameField;
     }
 }
